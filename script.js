4df56f714310ba8d1e9c97562e7f6b4997bf9395
Fabelixio <felixsewellknight@gmail.com>
2023-09-12 15:43:24 +0100

    add basic functions and initial button html

---
+++ script.js
@@ -0,0 +1,18 @@
+function addition(a, b) {
+    return a + b
+}
+function subtraction(a, b) {
+    return a - b
+}
+function multiplication(a, b) {
+    return a * b
+}
+function division(a, b) {
+    return a / b
+}
+let firstNum
+let secondNum
+let operator
+function operate(operator, firstNum, secondNum) {
+    //call above functions
+}
