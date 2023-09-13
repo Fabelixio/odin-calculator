d987d834aa974ffb4da51a14f91b97bd4bc715c7
Fabelixio <felixsewellknight@gmail.com>
2023-09-13 11:55:44 +0100

    add html and css styling to create calculator design

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 function addition(a, b) {
     return a + b
 }
-function subtraction(a, b) {
+function subtract(a, b) {
     return a - b
 }
-function multiplication(a, b) {
+function multiply(a, b) {
     return a * b
 }
 function division(a, b) {
     return a / b
 }
+//variables to update display
+
 let firstNum
 let secondNum
 let operator
-function operate(operator, firstNum, secondNum) {
-    //call above functions
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    switch(operator) {
+        case '+':
+            return addition(a, b)
+        case '-':
+            return subtract(a, b)
+        case 'x':
+            return multiply(a, b)
+        case '÷':
+            if (b === 0) return null
+            else return divide(a, b)
+        default:
+            return null
+    }
 }
+
+
+/* Fill out HTML with buttons, style initial calc with CSS, dont worry about linking to JS.
+build up calculator functions and link them to button presses to make the buttons
+the input into the function operations. */
